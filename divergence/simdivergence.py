7472cd361e16c09a0a146a3fbb238b8508d613a6
transferome <transferome1113@gmail.com>
2021-05-09 20:43:39 -0400

    test fetch

--- divergence/simdivergence.py
+++ divergence/simdivergence.py
@@ -1,6 +1,7 @@
 """Find the divergence between Gen0, and the Simulation replicates within the
 subregion simulation.freqs files, also, change row name to the region"""
 import fst.listsimfreqs as lfreqs
+import divergence.expdivergence as tools
 
 
 def region_and_file(freqfile):
@@ -10,5 +11,9 @@ def region_and_file(freqfile):
 
 
 def create_dict(freqfile):
+    reg, newf = region_and_file(freqfile)
     with open(freqfile) as f:
-        [line.rstrip('\n') for line in frepyqfile]
+        data = [line.rstrip('\n').split(',')[1:] for line in f]
+        gen0 = tools.float_freqs(data[0])
+        gen15 = [tools.float_freqs(sample) for sample in data[1:]]
+        
